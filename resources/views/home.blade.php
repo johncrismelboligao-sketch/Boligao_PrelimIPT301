cebe23568a90f2a89b20428ce650341b583450f6
BillionDolla <maatabillclipford@gmail.com>
2026-08-11 14:05:14 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1>Prelim Examination</h1>
+    </head>
+    <body>
+        <h1>MDC STUDENT ni sya</h1>
+        <p>John Crismel Boligao</p>
+        <p>BSIT-3 Section 2</p>
+        <p>Joga tayo mamaya?</p>            
+    </body>  
+</html> 
